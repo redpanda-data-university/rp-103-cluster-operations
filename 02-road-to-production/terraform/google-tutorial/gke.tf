2232675c40d5b3f2993fa467fa4a401366526844
Lalit K Narnaulia <k_lalit@outlook.com>
2023-06-29 14:48:19 +0100

    Fix https://github.com/redpanda-data-university/rp-103-cluster-operations/issues/2

--- 02-road-to-production/terraform/google-tutorial/gke.tf
+++ 02-road-to-production/terraform/google-tutorial/gke.tf
@@ -22,7 +22,7 @@ resource "google_container_node_pool" "redpanda_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-2"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
