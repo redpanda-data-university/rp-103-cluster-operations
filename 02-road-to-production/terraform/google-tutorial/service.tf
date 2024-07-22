8a288ed563c20aa5ffd5d52eb7ed3fb6c6561402
weimeilin79 <weimeilin@gmail.com>
2024-07-22 16:19:18 -0400

    Add last chapter

--- 02-road-to-production/terraform/google-tutorial/service.tf
+++
@@ -1,9 +0,0 @@
-# enable the iam service
-resource "google_project_service" "iam_service" {
-  service = "iam.googleapis.com"
-}
-
-# enable the container service
-resource "google_project_service" "container_service" {
-  service = "container.googleapis.com"
-}
